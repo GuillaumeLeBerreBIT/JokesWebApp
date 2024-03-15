5001c76517a6e3c587fa9351707b9bfce92a1927
Guillaume Le Berre <leberreguillaume.glb@gmail.com>
2024-03-15 16:18:21 +0100

    Initial commit

---
+++ Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        // Creating a property accesability
+        public int Id { get; set; } 
+        public String JokeQuestion { get; set; }    
+        public String JokeAnswer { get; set; }
+        
+        // Creating a constructor function
+        public Joke()
+        {
+            
+        }
+    }
+}
